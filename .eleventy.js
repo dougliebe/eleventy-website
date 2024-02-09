874d87809507e2c5afca1af37d21ba8a1dfc2643
dougliebe <liebe.doug@gmail.com>
2024-02-08 19:10:44 -0600

    Add favicon.ico to passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("favicon_io/favicon.ico");
     // the default is "copy"
     eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   };
