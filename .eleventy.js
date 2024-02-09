2b17586bb2abe12891df494f7e6c64b8869cd684
dougliebe <liebe.doug@gmail.com>
2024-02-08 19:42:19 -0600

    Update page titles

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
-    eleventyConfig.addPassthroughCopy("favicon_io/favicon.ico");
     // the default is "copy"
     eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   };
